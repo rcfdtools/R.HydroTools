eb6455eaae99c8b360f53519b9f049779c3e04cf
r.cfdtools <r.cfdtools@gmail.com>
2025-12-29 09:06:12 -0500

    General update.

--- tool/PMP/paper.py
+++ tool/PMP/paper.py
@@ -13,10 +13,13 @@ station_catalog_columns_drop = ['OBSERVACION', 'SUBRED'] # Dropped columns from 
 label_station = 'station' # Station column name to eval from .csv station dataset file
 label_station_catalog = 'CODIGO' # Station column code in CNE_IDEAM.xls
 label_name = 'NOMBRE' # Station column nome in CNE_IDEAM.xls
+label_category = 'CATEGORIA' # Station column category in CNE_IDEAM.xls
+label_technology = 'TECNOLOGIA' # Station column technology in CNE_IDEAM.xls
+label_active = 'ESTADO' # Station column active in CNE_IDEAM.xls
+label_install_date = 'FECHA_INSTALACION' # Station column activation date in CNE_IDEAM.xls
 label_latitude = 'LATITUD' # Station column latitude in CNE_IDEAM.xls
 label_longitude = 'LONGITUD' # Station column longitude in CNE_IDEAM.xls
-label_url = 'url' # Station link to .md report
-print_on_screen = True # Global print control in screen
+print_on_screen = False # Global print control in screen
 
 
 file_log_name = f'{output_path}{'paper'}.md'  # Markdown file log
@@ -42,22 +45,21 @@ df_stations = pd.DataFrame(stations, columns=[label_station])
 data_types = {label_station_catalog: 'str', label_latitude: 'float64', label_longitude: 'float64'}
 df_catalog = pd.read_excel(station_catalog_file, sheet_name='CNE', parse_dates=True, dtype=data_types) # , dtype=data_types
 df_catalog = df_catalog.drop(columns=station_catalog_columns_drop)
-#print(f'\ndf_catalog types: \n{df_catalog.dtypes}')
+print(f'\ndf_catalog types: \n{df_catalog.dtypes}')
 #print(df_catalog.head())
 df_catalog_filter = df_catalog[df_catalog[label_station_catalog].isin(df_stations[label_station])]
 #print(f'\nfiltered_df types: \n{filtered_df.dtypes}')
-df_catalog_filter[label_url] = 'Hello R.'
 df_catalog_filter = df_catalog_filter.reset_index(drop=True)
 #print(f'\n{df_catalog_filter.head().to_markdown()}')
 
 # Create GeoJSON map
-funcs.print_log(file_log, '\n\n## Location map\n\n')
+funcs.print_log(file_log, 'Map location', center_div=True, on_screen = print_on_screen)
 funcs.print_log(file_log, '```topojson\n{"type": "Topology", "objects": {"example": {"type": "GeometryCollection","geometries": [\n', on_screen = print_on_screen)
-selected_columns = df_catalog_filter[[label_station_catalog, label_name, label_latitude, label_longitude, label_url]]
+selected_columns = df_catalog_filter[[label_station_catalog, label_name, label_latitude, label_longitude, label_category]]
 for index, row in selected_columns.iterrows():
     #print (index)
     #print(f"Code: {row['CODIGO']}, Name: {row['NOMBRE']}")
-    properties = (f'"Code": "{row[label_station_catalog]}", "Name": "{row[label_name]}", "Latitude": "{row[label_latitude]}", "Longitude": "{row[label_longitude]}", "url": "{row[label_url]}"')
+    properties = (f'"Code": "{row[label_station_catalog]}", "Name": "{row[label_name]}", "Latitude": "{row[label_latitude]}", "Longitude": "{row[label_longitude]}", "Category": "{row[label_category]}"')
     print_geojson = '{"type": "Point","properties": {'+str(properties)+'},"coordinates": [' + str(row[label_longitude]) + ',' + str(row[label_latitude]) + ']}'
     funcs.print_log(file_log, print_geojson, on_screen = print_on_screen)
     if index <= len(df_catalog_filter) - 2:
