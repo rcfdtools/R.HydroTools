433cc298b18594619036385966090d73c0ff309a
r.cfdtools <r.cfdtools@gmail.com>
2025-12-29 08:26:09 -0500

    General update.

--- tool/PMP/paper.py
+++ tool/PMP/paper.py
@@ -12,9 +12,12 @@ station_catalog_file = 'dataset/CNE_IDEAM.xls' # CNE catalog for stations info
 station_catalog_columns_drop = ['OBSERVACION', 'SUBRED'] # Dropped columns from CNE
 label_station = 'station' # Station column name to eval from .csv station dataset file
 label_station_catalog = 'CODIGO' # Station column code in CNE_IDEAM.xls
+label_name = 'NOMBRE' # Station column nome in CNE_IDEAM.xls
 label_latitude = 'LATITUD' # Station column latitude in CNE_IDEAM.xls
 label_longitude = 'LONGITUD' # Station column longitude in CNE_IDEAM.xls
-print_on_screen = False
+label_url = 'url' # Station link to .md report
+print_on_screen = True # Global print control in screen
+
 
 file_log_name = f'{output_path}{'paper'}.md'  # Markdown file log
 file_log = open(file_log_name, 'w+', encoding='utf-8')   # w+ create the file if it doesn't exist
@@ -43,7 +46,24 @@ df_catalog = df_catalog.drop(columns=station_catalog_columns_drop)
 #print(df_catalog.head())
 df_catalog_filter = df_catalog[df_catalog[label_station_catalog].isin(df_stations[label_station])]
 #print(f'\nfiltered_df types: \n{filtered_df.dtypes}')
-funcs.print_log(file_log, f'\n\n## Stations\n\n{df_catalog_filter.to_markdown()}', center_div=False, on_screen = print_on_screen)
-print(f'\n{df_catalog_filter.head().to_markdown()}')
+df_catalog_filter[label_url] = 'Hello R.'
+df_catalog_filter = df_catalog_filter.reset_index(drop=True)
+#print(f'\n{df_catalog_filter.head().to_markdown()}')
 
-#
+# Create GeoJSON map
+funcs.print_log(file_log, '\n\n## Location map\n\n')
+funcs.print_log(file_log, '```topojson\n{"type": "Topology", "objects": {"example": {"type": "GeometryCollection","geometries": [\n', on_screen = print_on_screen)
+selected_columns = df_catalog_filter[[label_station_catalog, label_name, label_latitude, label_longitude, label_url]]
+for index, row in selected_columns.iterrows():
+    #print (index)
+    #print(f"Code: {row['CODIGO']}, Name: {row['NOMBRE']}")
+    properties = (f'"Code": "{row[label_station_catalog]}", "Name": "{row[label_name]}", "Latitude": "{row[label_latitude]}", "Longitude": "{row[label_longitude]}", "url": "{row[label_url]}"')
+    print_geojson = '{"type": "Point","properties": {'+str(properties)+',"coordinates": [' + str(row[label_longitude]) + ',' + str(row[label_latitude]) + ']}'
+    funcs.print_log(file_log, print_geojson, on_screen = print_on_screen)
+    if index <= len(df_catalog_filter) - 2:
+        funcs.print_log(file_log, ',\n', on_screen = print_on_screen)
+    else:
+        funcs.print_log(file_log, '\n', on_screen = print_on_screen)
+funcs.print_log(file_log, ']}}}\n\n```', on_screen = print_on_screen)
+
+#funcs.print_log(file_log, f'\n\n## Stations\n\n{df_catalog_filter.to_markdown()}', center_div=False, on_screen = print_on_screen)
