bfa130f9184712af53371a2bf02f5cb8536402ae
r.cfdtools <r.cfdtools@gmail.com>
2026-01-01 06:26:50 -0500

    General update.

--- tool/PMP/paper.py
+++ tool/PMP/paper.py
@@ -99,7 +99,7 @@ funcs.print_log(file_log, f'<img alt="R.HydroTools" src="{fig_file0a}" width="50
 print(f'\nFiltered stations catalog:\n{df_catalog_filter.to_markdown()}') # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 funcs.print_log(file_log, 'Stations list: ', center_div=False, on_screen = print_on_screen)
 for index, row in df_catalog_filter_selected_columns.iterrows():
-    station_url = (f'•[{row[label_station_catalog]}](../{row[label_station_catalog]}.md)')
+    station_url = (f'<sub>•[{row[label_station_catalog]}](../{row[label_station_catalog]}.md)</sub>')
     funcs.print_log(file_log, f'{station_url} ', on_screen=print_on_screen)
     '''
     if index <= len(df_catalog_filter) - 2:
