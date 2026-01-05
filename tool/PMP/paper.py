619e147edf3862332849fd86da6ec897cf612a4f
r.cfdtools <r.cfdtools@gmail.com>
2026-01-05 08:02:33 -0500

    General update.

--- tool/PMP/paper.py
+++ tool/PMP/paper.py
@@ -298,3 +298,5 @@ for i in range(best_fit_sort_eval): # for i in range(len(edf_dist)+1): or for i 
 
 # Footer
 funcs.print_log(file_log, f'\n\n<sub>{dictionary.dicts['disclaimer']}</sub>', on_screen = print_on_screen)
+
+
