784cb6beadd96ae3be40496fc7a4da128a5bd59a
r.cfdtools <r.cfdtools@gmail.com>
2026-01-05 08:47:45 -0500

    General update.

--- tool/PMP/paper.py
+++ tool/PMP/paper.py
@@ -260,7 +260,7 @@ for i in range(best_fit_sort_eval): # for i in range(len(edf_dist)+1): or for i 
         bars = ax.barh(empirical_dist_count['empirical_dist'], empirical_dist_count['Count'], color=color_plot)
         plt.yticks(rotation=0, ha='right')
         ax.bar_label(bars, padding=3)
-        ax.set_title(f'EDF - Empirical distribution (stations count for best fit # {i+1})')
+        ax.set_title(f'EDF - Empirical distribution (stations best fit # {i+1})')
         ax.set_xlabel('Count')
         plt.tight_layout() # Important >>> prevents cutting labels
         plt.subplots_adjust(bottom=0.075)
@@ -279,7 +279,7 @@ for i in range(best_fit_sort_eval): # for i in range(len(edf_dist)+1): or for i 
         bars = ax.bar(probability_dist_count['p_dist'], probability_dist_count['Count'], color=color_plot)
         plt.xticks(rotation=90, ha='right')
         ax.bar_label(bars, padding=3, rotation=90)
-        ax.set_title(f'PDF - Probability distribution function (stations count for best fit # {i+1})')
+        ax.set_title(f'PDF - Probability distribution function (stations best fit # {i+1})')
         ax.set_ylabel('Count')
         plt.tight_layout() # Important >>> prevents cutting labels
         plt.subplots_adjust(bottom=0.30)
@@ -302,7 +302,7 @@ for i in range(best_fit_sort_eval): # for i in range(len(edf_dist)+1): or for i 
             bars = ax.bar(probability_dist_count_ah['p_dist'], probability_dist_count_ah['Count'], color=color_plot)
             plt.xticks(rotation=90, ha='right')
             ax.bar_label(bars, padding=3, rotation=90)
-            ax.set_title(f'Hydrographic Area (AH): {ah}\nPDF - Probability distribution function (stations count for best fit # {i+1})')
+            ax.set_title(f'Hydrographic Area (AH): {ah}\nPDF - Probability distribution function (stations best fit # {i+1})')
             ax.set_ylabel('Count')
             plt.tight_layout() # Important >>> prevents cutting labels
             plt.subplots_adjust(bottom=0.30)
