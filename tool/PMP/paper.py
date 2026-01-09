50d9a0260828f06c20d84a6040c066a45a0ddeca
r.cfdtools <r.cfdtools@gmail.com>
2026-01-09 14:39:31 -0500

    General update.

--- tool/PMP/paper.py
+++ tool/PMP/paper.py
@@ -365,7 +365,7 @@ regular_hydrology_pdf_pdiff = [item + '_' + pdiff_suffix for item in regular_hyd
 funcs.print_log(file_log, f'\n{dictionary.dicts['pdiff']}', on_screen = print_on_screen)
 funcs.print_log(file_log, f' The most regular PDFs used in hydrology are : {', '.join(regular_hydrology_pdf)}.', center_div=False, on_screen = print_on_screen)
 funcs.print_log(file_log, f'\n This analysis include only stations with {minimum_sample_pdiff}+ yearly records.', on_screen = print_on_screen)
-funcs.print_log(file_log, f'\n\n:file_folder:Table: [extreme.csv](extreme.csv)', on_screen = print_on_screen)
+funcs.print_log(file_log, f'\n\n:file_folder:Table: [extreme.csv](extreme.csv)  ', on_screen = print_on_screen)
 funcs.print_log(file_log, f'\n:file_folder:Table: [extremepdiff.csv](extremepdiff.csv)', on_screen = print_on_screen)
 regular_hydrology_pdf_pdiff = ['n'] + regular_hydrology_pdf_pdiff
 # Analysis 1 - Stations % difference mean
