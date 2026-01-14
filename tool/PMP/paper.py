7dbef19e13cc25703f7cc276ced2f20d36032683
r.cfdtools <r.cfdtools@gmail.com>
2026-01-14 10:34:23 -0500

    General update.

--- tool/PMP/paper.py
+++ tool/PMP/paper.py
@@ -289,7 +289,7 @@ for i in range(best_fit_sort_eval): # for i in range(len(edf_dist)+1): or for i 
         bars = ax.barh(empirical_dist_count['empirical_dist'], empirical_dist_count['Count'], color=color_plot)
         plt.yticks(rotation=0, ha='right')
         ax.bar_label(bars, padding=3)
-        ax.set_title(f'EDF - Empirical distribution (stations best fit # {i+1}, {empirical_dist_count['Count'].sum()} stations)')
+        ax.set_title(f'EDF - Empirical distribution (stations best fit # {i+1} for {empirical_dist_count['Count'].sum()} stations)')
         ax.set_xlabel('Count')
         plt.tight_layout() # Important >>> prevents cutting labels
         plt.subplots_adjust(bottom=0.075)
@@ -308,7 +308,7 @@ for i in range(best_fit_sort_eval): # for i in range(len(edf_dist)+1): or for i 
         bars = ax.bar(probability_dist_count['p_dist'], probability_dist_count['Count'], color=color_plot)
         plt.xticks(rotation=90, ha='right')
         ax.bar_label(bars, padding=3, rotation=90)
-        ax.set_title(f'PDF - Probability distribution function (stations best fit # {i+1})')
+        ax.set_title(f'PDF - Probability distribution function (stations best fit # {i+1} for {probability_dist_count['Count'].sum()} stations)')
         ax.set_ylabel('Count')
         plt.tight_layout() # Important >>> prevents cutting labels
         plt.subplots_adjust(bottom=0.30)
