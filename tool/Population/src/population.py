022ab4c1a16981efa5ff83e69a9c17f415376876
r.cfdtools <r.cfdtools@gmail.com>
2026-04-02 13:33:56 -0500

    General update.

--- tool/Population/src/population.py
+++ tool/Population/src/population.py
@@ -7,12 +7,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # General vars
-file_path = '../data/Population.xlsx'
+file_path = '../data/PopulationTest.xlsx'
 county_id = '25899' # ● County code to be processed, 25899 - Zipaquirá, 15667 - San Luis de Gaceno
-population_col = 'PRural' # ● PTotal, PUrban, PRural
-projection_year_max = 2040 # ● Projection year
+population_col = 'PTotal' # ● PTotal, PUrban, PRural
+projection_year_max = 2051 # ● Projection year
+process_polynomial_d2_up = False # ● Doesn't recommend for population projections
 set_negative_to_zero = True
-show_plot = False # Show plot on Python screen console
+show_plot = True # Show plot on Python screen console
 
 # Processing
 dtype={'Year': int, 'CountyID': str, 'StateID': str, 'PTotal': int, 'PUrban': int, 'PRural': int}
@@ -38,29 +39,30 @@ if set_negative_to_zero: deg1_projection[deg1_projection < 0] = 0
 df_projected[f'{population_col}PD1'] =  deg1_projection
 print(f'* (PD1) Polynomial Deg 1: {coefficients_deg1[0]}, {coefficients_deg1[1]} (lineal)')
 
-# Polynomial projection Deg 2
-coefficients_deg2 = np.polyfit(filtered_df['Year'], filtered_df[population_col], deg=2)
-c1, c2, c3 = coefficients_deg2[0], coefficients_deg2[1], coefficients_deg2[2]
-deg2_projection = np.round(c1 * x_future**2 + c2 * x_future + c3, decimals=0)
-if set_negative_to_zero: deg2_projection[deg2_projection < 0] = 0
-df_projected[f'{population_col}PD2'] =  deg2_projection
-print(f'* (PD2) Polynomial Deg 2: {coefficients_deg2[0]}, {coefficients_deg2[1]}, {coefficients_deg2[2]}')
+if process_polynomial_d2_up:
+    # Polynomial projection Deg 2
+    coefficients_deg2 = np.polyfit(filtered_df['Year'], filtered_df[population_col], deg=2)
+    c1, c2, c3 = coefficients_deg2[0], coefficients_deg2[1], coefficients_deg2[2]
+    deg2_projection = np.round(c1 * x_future**2 + c2 * x_future + c3, decimals=0)
+    if set_negative_to_zero: deg2_projection[deg2_projection < 0] = 0
+    df_projected[f'{population_col}PD2'] =  deg2_projection
+    print(f'* (PD2) Polynomial Deg 2: {coefficients_deg2[0]}, {coefficients_deg2[1]}, {coefficients_deg2[2]}')
 
-# Polynomial projection Deg 3
-coefficients_deg3 = np.polyfit(filtered_df['Year'], filtered_df[population_col], deg=3)
-c1, c2, c3, c4 = coefficients_deg3[0], coefficients_deg3[1], coefficients_deg3[2], coefficients_deg3[3]
-deg3_projection = np.round(c1 * x_future**3 + c2 * x_future**2 + c3 * x_future + c4, decimals=0)
-if set_negative_to_zero: deg3_projection[deg3_projection < 0] = 0
-df_projected[f'{population_col}PD3'] =  deg3_projection
-print(f'* (PD3) Polynomial Deg 3: {coefficients_deg3[0]}, {coefficients_deg3[1]}, {coefficients_deg3[2]}, {coefficients_deg3[3]}')
+    # Polynomial projection Deg 3
+    coefficients_deg3 = np.polyfit(filtered_df['Year'], filtered_df[population_col], deg=3)
+    c1, c2, c3, c4 = coefficients_deg3[0], coefficients_deg3[1], coefficients_deg3[2], coefficients_deg3[3]
+    deg3_projection = np.round(c1 * x_future**3 + c2 * x_future**2 + c3 * x_future + c4, decimals=0)
+    if set_negative_to_zero: deg3_projection[deg3_projection < 0] = 0
+    df_projected[f'{population_col}PD3'] =  deg3_projection
+    print(f'* (PD3) Polynomial Deg 3: {coefficients_deg3[0]}, {coefficients_deg3[1]}, {coefficients_deg3[2]}, {coefficients_deg3[3]}')
 
-# Polynomial projection Deg 4
-coefficients_deg4 = np.polyfit(filtered_df['Year'], filtered_df[population_col], deg=4)
-c1, c2, c3, c4, c5 = coefficients_deg4[0], coefficients_deg4[1], coefficients_deg4[2], coefficients_deg4[3], coefficients_deg4[4]
-deg4_projection = np.round(c1 * x_future**4 + c2 * x_future**3 + c3 * x_future**2 + c4 * x_future + c5, decimals=0)
-if set_negative_to_zero: deg4_projection[deg4_projection < 0] = 0
-df_projected[f'{population_col}PD4'] =  deg4_projection
-print(f'* (PD4) Polynomial Deg 4: {coefficients_deg4[0]}, {coefficients_deg4[1]}, {coefficients_deg4[2]}, {coefficients_deg4[3]}, {coefficients_deg4[4]}')
+    # Polynomial projection Deg 4
+    coefficients_deg4 = np.polyfit(filtered_df['Year'], filtered_df[population_col], deg=4)
+    c1, c2, c3, c4, c5 = coefficients_deg4[0], coefficients_deg4[1], coefficients_deg4[2], coefficients_deg4[3], coefficients_deg4[4]
+    deg4_projection = np.round(c1 * x_future**4 + c2 * x_future**3 + c3 * x_future**2 + c4 * x_future + c5, decimals=0)
+    if set_negative_to_zero: deg4_projection[deg4_projection < 0] = 0
+    df_projected[f'{population_col}PD4'] =  deg4_projection
+    print(f'* (PD4) Polynomial Deg 4: {coefficients_deg4[0]}, {coefficients_deg4[1]}, {coefficients_deg4[2]}, {coefficients_deg4[3]}, {coefficients_deg4[4]}')
 
 # Logarithmic projection
 coefficients_logarithmic = np.polyfit(np.log(filtered_df['Year']), filtered_df[population_col], deg=1)
@@ -95,10 +97,18 @@ p_end = filtered_df.loc[t_end, population_col]
 total_growth = p_end - p_0
 elapsed_time = t_end - t_0
 annual_growth = total_growth / elapsed_time
-print(f'* (Art) Arithmetical: Year diff. {t_end} - {t_0} = {t_end-t_0}, Population diff. {p_end} - {p_0} = {p_end-p_0}, Annual growth {annual_growth}')
-arithmetical_projection = np.round(p_0+((x_future-t_0)*annual_growth), decimals=0)
-df_projected[f'{population_col}Art'] =  arithmetical_projection
-if set_negative_to_zero: arithmetical_projection[arithmetical_projection < 0] = 0
+print(f'* (Art) Arithmetic: Year diff. {t_end} - {t_0} = {t_end-t_0}, Population diff. {p_end} - {p_0} = {p_end-p_0}, Annual growth {annual_growth}')
+arithmetic_projection = np.round(p_0+((x_future-t_0)*annual_growth), decimals=0)
+df_projected[f'{population_col}Art'] =  arithmetic_projection
+if set_negative_to_zero: arithmetic_projection[arithmetic_projection < 0] = 0
+
+# Geometric projection
+annual_growth = (p_end/p_0) ** (1 / elapsed_time) - 1
+print(f'* (Geo) Geometric: Year diff. {t_end} - {t_0} = {t_end-t_0}, Population diff. {p_end} - {p_0} = {p_end-p_0}, Annual growth % {annual_growth}')
+geometric_projection = np.round(p_end*(1+annual_growth)**(x_future-t_end), decimals=0)
+df_projected[f'{population_col}Geo'] =  geometric_projection
+if set_negative_to_zero: geometric_projection[geometric_projection < 0] = 0
+
 
 # Print and plot results
 print(f'\n\n# Dataset\n\n{df_projected.to_markdown(index=False)}')
@@ -107,13 +117,15 @@ if show_plot:
     #plt.scatter(filtered_df['Year'], filtered_df[population_col], color='black', label='Censal Data')
     plt.scatter(filtered_df.index, filtered_df[population_col], color='black', label='Censal Data')
     plt.plot(x_future, deg1_projection, color='black', linestyle='--', lw=1, label=f'Polynomial D1 ({int(deg1_projection[-1])})')
-    plt.plot(x_future, deg2_projection, color='black', linestyle='-.', lw=1, label=f'Polynomial D2 ({int(deg2_projection[-1])})')
-    plt.plot(x_future, deg3_projection, color='black', linestyle=':', lw=1, label=f'Polynomial D3 ({int(deg3_projection[-1])})')
-    plt.plot(x_future, deg4_projection, color='green', linestyle='--', lw=1, label=f'Polynomial D4 ({int(deg4_projection[-1])})')
+    if process_polynomial_d2_up:
+        plt.plot(x_future, deg2_projection, color='black', linestyle='-.', lw=1, label=f'Polynomial D2 ({int(deg2_projection[-1])})')
+        plt.plot(x_future, deg3_projection, color='black', linestyle=':', lw=1, label=f'Polynomial D3 ({int(deg3_projection[-1])})')
+        plt.plot(x_future, deg4_projection, color='green', linestyle='--', lw=1, label=f'Polynomial D4 ({int(deg4_projection[-1])})')
     plt.plot(x_future, logarithmic_projection, color='green', linestyle='-.', lw=1, label=f'Logarithmic ({int(logarithmic_projection[-1])})')
     plt.plot(x_future, potential_projection, color='green', linestyle=':', lw=1, label=f'Potential ({int(potential_projection[-1])})')
     plt.plot(x_future, exponential_projection, color='orange', linestyle='--', lw=1, label=f'Exponential ({int(exponential_projection[-1])})')
-    plt.plot(x_future, arithmetical_projection, color='orange', linestyle='-.', lw=1, label=f'Arithmetical ({int(arithmetical_projection[-1])})')
+    plt.plot(x_future, arithmetic_projection, color='orange', linestyle='-.', lw=1, label=f'Arithmetic ({int(arithmetic_projection[-1])})')
+    plt.plot(x_future, geometric_projection, color='orange', linestyle=':', lw=1, label=f'Geometric ({int(geometric_projection[-1])})')
     #plt.plot(filtered_df['Year'], p(filtered_df['Year']), color='red', lw=1, label='Fitted Line')
     plt.xlabel('Year')
     plt.ylabel('Population')
