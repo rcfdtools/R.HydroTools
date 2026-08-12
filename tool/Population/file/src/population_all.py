aff0a871216be4760f14e4ccb5dc3b440311ff7f
r.cfdtools <r.cfdtools@gmail.com>
2026-08-12 17:02:34 -0500

    General update.

--- tool/Population/file/src/population_all.py
+++ tool/Population/file/src/population_all.py
@@ -96,8 +96,9 @@ for county_id in county_list:
     funcs.print_log(file_log, f'\n\n{filtered_df.sort_values(by='Year').to_markdown(index=False)}')
     funcs.print_log(file_log, f'\n\n> 🔥Some records could had specific notes about the registered values or the corresponding urban or rural distribution.')
     if len(df_shapefile) > 0:
-        print()
-        funcs.print_log(file_log, f'\n\n## Local public utility companies in {state_name_unicode.upper()} - {county_name_unicode.upper()} \n\n {df_rups_county.to_markdown(index=False)}')
+        df_rups_county = df_rups_county.drop(columns=[rups_state_name_var, rups_county_name_var])
+        funcs.print_log(file_log, f'\n\n## Local public utility companies (RUPS) in {state_name_unicode.upper()} - {county_name_unicode.upper()} \n\n {df_rups_county.to_markdown(index=False)}')
+        funcs.print_log(file_log, f'\n\n> [📅RUPS Database: Registro Único de Prestadores de Servicios Públicos de Colombia Suramérica - RUPS](https://www.datos.gov.co/Hacienda-y-Cr-dito-P-blico/Registro-nico-de-Prestadores-de-Servicios-P-blicos/4qkq-csdn/about_data)')
 
     # Processing by zone
     num = 1 # Counter required for contents table index
